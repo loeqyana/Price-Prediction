bf57f683a390889d62a4431ef0b8b14765bd7af7
Luqyana Nurul <145703882+loeqyana@users.noreply.github.com>
2026-08-12 17:49:10 +0700

    Enhance app.py to list installed packages
    Added subprocess and sys imports to list installed packages.

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import subprocess
+import sys
 
 st.write("Pandas OK")
+
+
+st.text(subprocess.check_output([sys.executable, "-m", "pip", "list"]).decode())
